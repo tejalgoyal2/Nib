b47ecb6aa86cfbb565d3bb3cd9c74c628b1026c4
tejal <itejalgoyal@gmail.com>
2026-04-30 15:33:52 -0700

    Initial Commit

---
+++ Nib/NibApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibApp.swift
+//  Nib
+//
+//  Created by Tejal Goyal on 2026-04-30.
+//
+
+import SwiftUI
+
+@main
+struct NibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
